4d9f13099efa83dfbcf8b24ee283621f89bc29a1
DanielekPark <54155014+DanielekPark@users.noreply.github.com>
2020-04-27 13:28:32 -0700

    html-css-js additions

--- js/app.js
+++ js/app.js
@@ -2,10 +2,14 @@ document.addEventListener('DOMContentLoaded', () => {
 /* ===================================== 
   VARIABLES
 ======================================== */
-const gridContainer = document.querySelector('.grid-container')
-const close = document.querySelector(".close");
+let employees = [];
+const urlAPI = 'https://randomuser.me/api/?results=12&inc=name,%20picture,%20email,%20location,%20phone,%20dob%20&noinfo%20&nat=US'
+const gridContainer = document.querySelector('.grid-container');
 const info = document.querySelector('#info');
-const cards = document.querySelectorAll('.card'); 
+const modal = document.querySelector('.modal'); //overlay
+const modalContent = document.querySelector('.modal-content'); 
+const cards = document.querySelectorAll('.card');
+const close = document.querySelector(".close");
 
 /* ===================================== 
    FUNCTIONS
@@ -14,39 +18,65 @@ const show = event => {
   info.style.display = 'block'; 
 }
 
-function fetchData(url){
-    return fetch(url)
-              .then(checkStatus)
-              .then(res => res.json() )
-              .catch(error => console.log('Looks like there was problem', error) )
-  }
-
 function checkStatus(response){
-    if(response.ok){
-      return Promise.resolve(response);
-    }else{
-      return Promise.reject(response.statusText)
-    }
-  } 
+  if(response.ok){
+    return Promise.resolve(response)
+  }else{
+    return Promise.reject(new Error (response.statusText) )
+  }
+}
+
+function fetchData(url){
+  return fetch(url)
+         .then(checkStatus)
+         .then(response => response.json() )
+         .catch(error => console.log('Looks like there was a problem', error) ); 
+}
+
+function displayEmployees(employeeData){
+  employees = employeeData
+  
+  let employeeHTML = ''; 
+
+  employees.forEach((employee, index) => {
+    let name = employee.name; 
+    let email = employee.email; 
+    let city = employee.location.city;
+    let pictures = employee.picture;
+
+    employeeHTML += 
+      `<div class="card" data-index="${index}">
+          <img class="avatar" src="${pictures.thumbnail}">
+          <div class="text-container">
+            <p class="name">${name.first} ${name.last}</p>
+            <p class="email">${email}</p>
+            <p class="city">${city}</p>
+          </div>
+      </div>` 
+    
+  })
+
+  gridContainer.innerHTML = employeeHTML;
+  
+}
 
 /* ===================================== 
    EVENT LISTENERS
 ======================================== */
-cards.forEach(card => card.addEventListener('click', show))
 
-// gridContainer.addEventListener('click', event => {
-//   if(event.target.className === 'card'){
-//      info.style.display = 'block'
-//      }
-// });
 
-close.addEventListener('click', () => {
-  info.style.display = 'none'; 
-}); 
 
 /* ===================================== 
- FETCH
+ FETCH REQUESTS
 ======================================== */
+fetchData(urlAPI)
+  .then(data => data.results)
+  //.then(data => console.log(data))
+  .then(data => displayEmployees(data))
 
+
+  
+
+  
 
 })
